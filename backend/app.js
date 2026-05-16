a054048561960648fc30a70e4150dd44369a5038
Amanpreet Kaur <aman@gmail.com>
2026-05-16 11:34:39 +0530

    add

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(express.json());
 // app.use(cors());
 app.use(cors({
-    origin: ["http://localhost:5173", ""]
+    origin: ["http://localhost:5173", "https://wooden-frontend.onrender.com"]
 }));
 app.use(fileUpload());
 
